c843bffa69512e6b66282cad2c8cdad72e4f2286
justelnegro@gmail.com <justelnegro@gmail.com@696b72fa-092e-11df-aae1-e754eacfc418>
2010-02-01 21:56:39 +0000

    Add the method decToBin

--- trunk/Api_1_0/com/api/src/Core.cpp
+++ trunk/Api_1_0/com/api/src/Core.cpp
@@ -29,6 +29,52 @@ namespace api{
 	{
 	}
 	
+	/***********************************************************************/	
+	/************************Funcion decToBinr******************************/	
+	/***Toma un areglo de enteros y devuelve su representacion binaria******/
+	/**********Usando el compelemento a 2***********************************/	
+	
+	int *decToBin(int decimal, int nBits, int binary[]) {
+	    int i;
+	
+	    int flag = 0;
+	    /* Si es un número negativo, lo hacemos positivo y le restamos 1(*), */
+	    /* además activamos el flag para saber que es negativo */
+	
+	    /* (*) El complemento a 2 es igual que el complemento a 1, que es cambiar */
+	    /* 1->0, 0->1, y luego sumando 1 en binario. Aquí restamos 1 en decimal y */
+	    /* luego pasamos a binario. */
+	    if (decimal < 0) 
+	        {
+	            flag = 1;
+	            decimal = -decimal-1;
+	        }
+	
+	    /* Pasamos a binario, atendiendo a lo que vale flag, en lugar de añadir (decimal%2) */
+	    /* como es normal, le hacemos un XOR con flag, vemos la tabla de verdad de XOR */
+	    /* |    A    (xor)   B       =   X   |  */
+	    /* |     0      |        0       |   0   |  */
+	    /* |     0      |        1       |   1   |  */
+	    /* |     1      |        0       |   1   |  */
+	    /* |     1      |        1       |   0   |  */
+	    /* ------------------------------   */
+	    /* Por tanto, cuando flag vale 0, al encontrar un 0, pondremos un 0, y al encontrar un 1, ponemos un 1 */
+	    /*           Pero cuando flag vale 1, al encontrar un 0, pondremos un 1, y al encontrar un 1, ponemos un 0 */
+	    while (decimal > 0) 
+	        {
+	            binary[--nBits] = flag^(decimal%2);
+	            decimal/=2;
+	        }
+	
+	    /* Con esto terminamos de llenar el array, con 0 a la izquierda si es un número positivo */
+	    /* y con 1s si es un número negativo */
+	    while (nBits>0)
+	        binary[--nBits] = flag;
+	    
+	    /* Devolvemos el array */
+	    return binary;
+	}
+	
  	/***********************************************************************/	
 	/************************Funcion Conversor*****************************/	
 	/***Toma un caracter y lo convierte en un String de 8 posiciones*******/
@@ -38,37 +84,33 @@ namespace api{
 	{  
 		try{ 
 		
-			int base1=2, base2=10;	
+			int n1=(int)c;
 			
-			unsigned long alg,mult=1,n1=(int)c,n2=0;
+			int *n2;
+			
+			int n3[8];
 		 	
-		 	string val; 
-		   
-				while (n1 > 0) 
-			   	{ 
-			     	alg = n1 % base1; 
-			      	
-			      	n1 /= base1; 
-			      	
-			      	n2 += (alg*mult); 
-			      	
-			      	mult *= base2; 
-			   	}
-		   	
-			   	std::stringstream ss;
-			  	
-			  	ss << n2;
-			  	
-			  	val=ss.str();
-		  	
-			  	while (val.size()<8)
-			  	{
-			  	
-			  	val="0"+val;
-			  	
-			  	}
-		   
-		   		return val; 
+		 	string val="";
+		 	
+		 	n2=decToBin(n1,8,n3);
+   	
+	
+			for (int i = 0; i< 8 ; i++)
+			{
+				if (n3[i] == 1)
+				{
+					val = val + "1" ;
+				}
+				else
+					if(n3[i] == 0) 
+					{
+					
+					val=val +"0";
+					
+					}
+			}
+	
+   			return val; 
 	   }
 	   catch(...)
 	   {
