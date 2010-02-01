cb0c10e2240b1bd16a8beef45d8c960575bacf97
justelnegro@gmail.com <justelnegro@gmail.com@696b72fa-092e-11df-aae1-e754eacfc418>
2010-02-01 06:51:28 +0000

    Add the class Sniffer that has the implementation from TCPDUMP for the sniffer

---
+++ trunk/Api_1_0/com/api/src/Sniffer.h
@@ -0,0 +1,44 @@
+/*
+ * sniffex.c
+ *
+ * Sniffer example of TCP/IP packet capture using libpcap.
+ * 
+ * Version 0.1.1 (2005-07-05)
+ * Copyright (c) 2005 The Tcpdump Group
+ *
+ * This software is intended to be used as a practical example and 
+ * demonstration of the libpcap library; available at:
+ * http://www.tcpdump.org/ */
+
+
+#ifndef SNIFFER_H_
+#define SNIFFER_H_
+#define APP_NAME		"sniffex"
+#define APP_DESC		"Sniffer example using libpcap"
+#define APP_COPYRIGHT	"Copyright (c) 2005 The Tcpdump Group"
+#define APP_DISCLAIMER	"THERE IS ABSOLUTELY NO WARRANTY FOR THIS PROGRAM."
+
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iostream>
+namespace api
+{
+
+class Sniffer
+{
+	public:
+		Sniffer();
+		virtual ~Sniffer();
+		};
+
+}
+
+#endif /*SNIFFER_H_*/
