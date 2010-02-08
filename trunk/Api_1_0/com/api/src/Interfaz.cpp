d1cc77bdcb18b72a2911a314bc0c88aa1edcb4a4
alexnewski <alexnewski@696b72fa-092e-11df-aae1-e754eacfc418>
2010-02-08 04:24:28 +0000

    interjase

--- trunk/Api_1_0/com/api/src/Interfaz.cpp
+++ trunk/Api_1_0/com/api/src/Interfaz.cpp
@@ -1,10 +1,11 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <vector>
 #include "Interfaz.h"
 
 
@@ -13,7 +14,7 @@ namespace api {
 
 
     //Clase que contiene el procesamiento de paquetes para ser llevado a la inteligencia
-    // artificial para su posterior identificacion 
+    // artificial para su posterior identificacion
 
     using namespace std;
 
@@ -23,8 +24,16 @@ namespace api {
     /*********************************************************************/
     /*********************************************************************/
 
+    /*    bool Interfaz::act_msn;
+
+        bool Interfaz::act_torrent;*/
+
     /*******Contructor********/
-    Interfaz::Interfaz() {
+    Interfaz::Interfaz(bool msn, bool torrent) {
+
+        this->act_msn = msn;
+        this->act_torrent = torrent;
+
     }
 
     /******Destructor*******/
@@ -42,6 +51,8 @@ namespace api {
             printf("/                                                           /\n");
             printf("/           2.Excepciones IP                                /\n");
             printf("/                                                           /\n");
+            printf("/           3.Salir                                         /\n");
+            printf("/                                                           /\n");
             printf("/                                                           /\n");
             printf("/***********************************************************/\n");
             printf("/*****           Por favor elegir una opcion           *****/\n");
@@ -49,14 +60,16 @@ namespace api {
 
             int n;
             scanf("%d", &n);
-            fflush(stdout);
+            system("clear");
 
             switch (n) {
                 case 1:
                     return Aplicaciones();
                     break;
                 case 2:
-                    printf("opcion 2\n");
+                    return ExcepcionesIP();
+                    break;
+                case 3:
                     break;
                 default:
                     printf("opcion incorrecta\n");
@@ -81,7 +94,7 @@ namespace api {
         printf("/                                                           /\n");
         printf("/           2.MSN Messenger                                 /\n");
         printf("/                                                           /\n");
-        printf("/           3.Salir                                         /\n");        
+        printf("/           3.Salir                                         /\n");
         printf("/                                                           /\n");
         printf("/***********************************************************/\n");
         printf("/*****           Por favor elegir una opcion           *****/\n");
@@ -89,18 +102,80 @@ namespace api {
 
         int n;
         scanf("%d", &n);
-
+        system("clear");
 
         switch (n) {
             case 1:
-                return AppOpciones((char *)"Torrent      ");
+                return AppOpciones(n);
                 break;
             case 2:
-                return AppOpciones((char *)"MSN Messenger");
+                return AppOpciones(n);
                 break;
             case 3:
                 return Inicio();
-                break;    
+                break;
+            default:
+                printf("opcion incorrecta\n");
+                return Aplicaciones();
+                break;
+        }
+        return 0;
+    }
+
+    int Interfaz::AppOpciones(int app) {
+
+        printf("/***********************************************************/\n");
+        if (app == 1)
+            printf("/*****             Filtro Torrent                      *****/\n");
+        if (app == 2)
+            printf("/*****             Filtro MSN Messenger                *****/\n");
+        printf("/***********************************************************/\n");
+        printf("/                                                           /\n");
+        printf("/                                                           /\n");
+        if (((app == 1) && (this->act_torrent == false)) || ((app == 2) && (this->act_msn == false))) {
+            printf("/           El estado actual es APAGADO                     /\n");
+            printf("/                                                           /\n");
+            printf("/           1.Activar                                       /\n");
+        }
+        if (((app == 1) && (this->act_torrent == true)) || ((app == 2) && (this->act_msn == true))) {
+            printf("/           El estado actual es ENCENDIDO                   /\n");
+            printf("/                                                           /\n");
+            printf("/           1.Desactivar                                    /\n");
+        }
+        printf("/                                                           /\n");
+        printf("/           2.Salir                                         /\n");
+        printf("/                                                           /\n");
+        printf("/                                                           /\n");
+        printf("/***********************************************************/\n");
+        printf("/*****           Por favor elegir una opcion           *****/\n");
+        printf("/***********************************************************/\n");
+
+        int n;
+        scanf("%d", &n);
+        system("clear");
+        switch (n) {
+            case 1:
+                if (((app == 1) && (this->act_torrent == false)) || ((app == 2) && (this->act_msn == false))) {
+                    if (app == 1)
+                        this->act_torrent = true;
+                    if (app == 2)
+                        this->act_msn = true;
+                    return Aplicaciones();
+                }
+
+
+                if (((app == 1) && (this->act_torrent == true)) || ((app == 2) && (this->act_msn == true))) {
+                    if (app == 1)
+                        this->act_torrent = false;
+                    if (app == 2)
+                        this->act_msn = false;
+                    return Aplicaciones();
+                }
+
+                break;
+            case 2:
+                return Aplicaciones();
+                break;
             default:
                 printf("opcion incorrecta\n");
                 break;
@@ -108,16 +183,160 @@ namespace api {
         return 0;
     }
 
-    int Interfaz::AppOpciones(char *app) {
+    int Interfaz::ExcepcionesIP() {
+
         printf("/***********************************************************/\n");
-        printf("/*****             Filtro %s                *****/\n",app);
+        printf("/*****                Excepciones IP                   *****/\n");
         printf("/***********************************************************/\n");
         printf("/                                                           /\n");
         printf("/                                                           /\n");
-        printf("/           1.Activar                                       /\n");
+        printf("/           1.Agregar                                       /\n");
         printf("/                                                           /\n");
-        printf("/           2.Desactivar                                    /\n");
+        printf("/           2.Eliminar                                      /\n");
         printf("/                                                           /\n");
+        printf("/           3.Salir                                         /\n");
+        printf("/                                                           /\n");
+        printf("/           Listado de IP                                   /\n");
+        printf("/                                                           /\n");
+
+
+        for (int i = 0; i < ip_exc.size(); i++) {
+            if ((ip_exc.at(i).length()) > 0) {
+                string formato = ip_exc.at(i);
+                for (int j = formato.length(); j < 16; j++) {
+                    formato = formato + " ";
+                }
+
+                cout << "/           " + formato + "                                /\n";
+            }
+        }
+
+        printf("/                                                           /\n");
+        printf("/***********************************************************/\n");
+        printf("/*****           Por favor elegir una opcion           *****/\n");
+        printf("/***********************************************************/\n");
+
+        int n;
+        scanf("%d", &n);
+        system("clear");
+
+        switch (n) {
+            case 1:
+                return AgregarIP();
+                break;
+            case 2:
+                if (ip_exc.size() > 0)
+                    return EliminarIP();
+                else {
+                    printf("/***********************************************************/\n");
+                    printf("/*****          No hay ninguna IP en lista             *****/\n");
+                    printf("/***********************************************************/\n");
+                    return ExcepcionesIP();
+                }
+                break;
+            case 3:
+                return Inicio();
+                break;
+            default:
+                printf("opcion incorrecta\n");
+                return Aplicaciones();
+                break;
+        }
+        return 0;
+
+    }
+
+    int Interfaz::AgregarIP() {
+        printf("/***********************************************************/\n");
+        printf("/*****            Introduzca una IP valida             *****/\n");
+        printf("/***********************************************************/\n");
+
+        string ip;
+        cin >> ip;
+        system("clear");
+
+        int i = 0;
+        int tope = 0;
+        for (int i = 0; i < this->ip_exc.size(); i++) {
+            if ((this->ip_exc.at(i)) == ip) {
+                printf("/***********************************************************/\n");
+                printf("/*****         Ya habia sido almacenada su IP          *****/\n");
+                printf("/***********************************************************/\n");
+                printf("\n");
+
+                return ExcepcionesIP();
+
+            }
+            i++;
+        }
+
+        this->ip_exc.push_back(ip);
+
+        printf("/***********************************************************/\n");
+        printf("/*****           IP Guardada Exitosamente              *****/\n");
+        printf("/***********************************************************/\n");
+        printf("\n");
+
+
+        return ExcepcionesIP();
+
+    }
+
+    int Interfaz::EliminarIP() {
+
+        int tope;
+
+        printf("/***********************************************************/\n");
+        printf("/*****                Eliminar IP                      *****/\n");
+        printf("/***********************************************************/\n");
+        printf("/                                                           /\n");
+
+        for (int i = 0; i < this->ip_exc.size(); i++) {
+            string formato = ip_exc.at(i);
+            for (int j = formato.length(); j < 16; j++) {
+                formato = formato + " ";
+            }
+            printf("/    %i)    ", i + 1);
+            cout << formato + "                                 /\n";
+            tope = i;
+        }
+
+        printf("/                                                           /\n");
+        printf("/***********************************************************/\n");
+        printf("/*****           Por favor elegir una opcion           *****/\n");
+        printf("/***********************************************************/\n");
+
+        int n;
+        scanf("%d", &n);
+
+        system("clear");
+
+        if (n <= tope + 1)
+            return ValidarEliminar(n - 1);
+        else
+            return ExcepcionesIP();
+
+
+    }
+
+    int Interfaz::ValidarEliminar(int val) {
+
+        printf("/***********************************************************/\n");
+        printf("/*****                Eliminar IP                      *****/\n");
+        printf("/***********************************************************/\n");
+        printf("/                                                           /\n");
+        printf("/    ¿Esta seguro que desea eliminar la IP?                 /\n");
+
+        string formato = ip_exc.at(val);
+        for (int j = formato.length(); j < 16; j++) {
+            formato = formato + " ";
+        }
+
+        cout << "/           " + formato + "                                /\n";
+        printf("/                                                           /\n");
+        printf("/           1.Si                                            /\n");
+        printf("/                                                           /\n");
+        printf("/           2.No                                            /\n");
         printf("/                                                           /\n");
         printf("/***********************************************************/\n");
         printf("/*****           Por favor elegir una opcion           *****/\n");
@@ -128,16 +347,27 @@ namespace api {
 
         switch (n) {
             case 1:
-                return Aplicaciones();
+                for (int i = 0; i < this->ip_exc.size(); i++) {
+                    if (val == i)
+                        ip_exc.erase(ip_exc.begin() + val);
+
+                }
+                return ExcepcionesIP();
                 break;
             case 2:
-                printf("opcion 2\n");
+                return ExcepcionesIP();
+                break;
+            case 3:
+                return Inicio();
                 break;
             default:
                 printf("opcion incorrecta\n");
+                return ExcepcionesIP();
                 break;
         }
         return 0;
     }
 
 }
+
+
